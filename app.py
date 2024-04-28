780b4a4966424837bcee97227d3374795a56215c
Lev Antuganov <antgnff@gmail.com>
2024-04-28 22:13:59 +0400

    4

--- app.py
+++ app.py
@@ -42,15 +42,22 @@ class Inventory(db.Model):
 with app.app_context():
     db.create_all()
 
+
 class AddToInventoryForm(FlaskForm):
     location_id = QuerySelectField(
         get_label='name',
         validators=[DataRequired()],
-        query_factory=lambda: Location.query.filter(                        
-            ~Location.inventories.any(                                      
-                Inventory.product_id == request.form.get('product_id'))
-        )
+        query_factory=lambda product_id: Location.query.filter(
+            ~Location.inventories.any(
+                Inventory.product_id == product_id)
+        ),
     )
+
+    def __init__(self, product_id, *args, **kwargs):
+        super(AddToInventoryForm, self).__init__(*args, **kwargs)
+        self.product_id = product_id
+        self.location_id.query = self.location_id.query_factory(
+            self.product_id)
 
 
 class DeleteFromInventoryForm(FlaskForm):
@@ -84,7 +91,8 @@ class ChangeQuantityForm(FlaskForm):
 def warehouse():
     return render_template('warehouse.html', products=Product.query.all(), locations=Location.query.all(),
                            add_product_form=AddProductForm(), add_location_form=AddLocationForm(),
-                           add_to_inventory_form=AddToInventoryForm(), delete_from_inventory_form=DeleteFromInventoryForm(), change_quantity_form=ChangeQuantityForm())
+                           add_to_inventory_form=AddToInventoryForm, delete_from_inventory_form=DeleteFromInventoryForm(),
+                           change_quantity_form=ChangeQuantityForm())
 
 
 @app.route('/add_product', methods=['POST'])
@@ -99,7 +107,9 @@ def add_product():
         db.session.add(product)
         db.session.commit()
         return render_template('table.html', products=Product.query.all(), locations=Location.query.all(),
-                               add_to_inventory_form=AddToInventoryForm(), delete_from_inventory_form=DeleteFromInventoryForm(), change_quantity_form=ChangeQuantityForm())
+                               add_product_form=AddProductForm(), add_location_form=AddLocationForm(),
+                               add_to_inventory_form=AddToInventoryForm, delete_from_inventory_form=DeleteFromInventoryForm(),
+                               change_quantity_form=ChangeQuantityForm())
 
 
 @app.route('/add_location', methods=['POST'])
@@ -112,22 +122,26 @@ def add_location():
         db.session.add(location)
         db.session.commit()
         return render_template('table.html', products=Product.query.all(), locations=Location.query.all(),
-                               add_to_inventory_form=AddToInventoryForm(), delete_from_inventory_form=DeleteFromInventoryForm(), change_quantity_form=ChangeQuantityForm())
+                               add_product_form=AddProductForm(), add_location_form=AddLocationForm(),
+                               add_to_inventory_form=AddToInventoryForm, delete_from_inventory_form=DeleteFromInventoryForm(),
+                               change_quantity_form=ChangeQuantityForm())
 
 
 @app.route('/add_to_inventory', methods=['POST'])
 def add_to_inventory():
-    form = AddToInventoryForm(request.form)
     product_id = request.form.get('product_id')
-    if form.validate_on_submit():
+    add_to_inventory_form = AddToInventoryForm(product_id=product_id)
+    if add_to_inventory_form.validate_on_submit():
         inventory = Inventory(
             product_id=product_id,
-            location_id=form.location_id.data.id,
+            location_id=add_to_inventory_form.location_id.data.id,
         )
         db.session.add(inventory)
         db.session.commit()
         return render_template('table.html', products=Product.query.all(), locations=Location.query.all(),
-                               add_to_inventory_form=AddToInventoryForm(), delete_from_inventory_form=DeleteFromInventoryForm(), change_quantity_form=ChangeQuantityForm())
+                               add_product_form=AddProductForm(), add_location_form=AddLocationForm(),
+                               add_to_inventory_form=AddToInventoryForm, delete_from_inventory_form=DeleteFromInventoryForm(),
+                               change_quantity_form=ChangeQuantityForm())
 
 
 @app.route('/delete_from_inventory', methods=['POST'])
@@ -141,7 +155,9 @@ def delete_from_inventory():
             db.session.delete(inventory)
             db.session.commit()
             return render_template('table.html', products=Product.query.all(), locations=Location.query.all(),
-                                   add_to_inventory_form=AddToInventoryForm(), delete_from_inventory_form=DeleteFromInventoryForm(), change_quantity_form=ChangeQuantityForm())
+                                   add_product_form=AddProductForm(), add_location_form=AddLocationForm(),
+                                   add_to_inventory_form=AddToInventoryForm, delete_from_inventory_form=DeleteFromInventoryForm(),
+                                   change_quantity_form=ChangeQuantityForm())
 
 
 @app.route('/change_quantity', methods=['POST'])
@@ -154,8 +170,10 @@ def change_quantity():
         if inventory:
             inventory.quantity = int(form.quantity.data)
             db.session.commit()
-        return render_template('table.html', products=Product.query.all(), locations=Location.query.all(),
-                               add_to_inventory_form=AddToInventoryForm(), delete_from_inventory_form=DeleteFromInventoryForm(), change_quantity_form=ChangeQuantityForm())
+            return render_template('table.html', products=Product.query.all(), locations=Location.query.all(),
+                                   add_product_form=AddProductForm(), add_location_form=AddLocationForm(),
+                                   add_to_inventory_form=AddToInventoryForm, delete_from_inventory_form=DeleteFromInventoryForm(),
+                                   change_quantity_form=ChangeQuantityForm())
 
 
 if __name__ == '__main__':
